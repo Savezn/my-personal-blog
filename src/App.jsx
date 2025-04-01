c8a9bccfdce83b2b2bbb5587c674c12c4e1ccd9c
Nut.Thakit <natthakitzn@gmail.com>
2025-04-01 19:45:06 +0700

    feat(router): install react-router-dom and extract LandingPage component from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
-import HeroSection from "./components/HeroSection";
-import Footer from "./components/Footer";
-import { ArticleSection } from "./components/ArticleSection";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-background text-text font-sans mx-auto m-0 p-0 max-w-full">
-      <NavBar />
-      <HeroSection />
-      <ArticleSection />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,17 @@
+import NavBar from "../components/NavBar";
+import HeroSection from "../components/HeroSection";
+import Footer from "../components/Footer";
+import { ArticleSection } from "../components/ArticleSection";
+
+const LandingPage = () => {
+return (
+    <div className="flex flex-col items-center min-h-screen bg-background text-text font-sans mx-auto m-0 p-0 max-w-full">
+      <NavBar />
+      <HeroSection />
+      <ArticleSection />
+      <Footer />
+    </div>
+)
+}
+
+export default LandingPage
